486cdad5a7f46d37651066e585023914fac0e74a
jarsp <jarsp>
2022-01-11 00:53:19 +0800

    refactoring

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -33,7 +33,7 @@ impl FromStr for Signal {
     type Err = Box<dyn Error>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let vs: Vec<Vec<String>> =
+        let mut vs: Vec<Vec<String>> =
             s.split(" | ")
              .map(|t| {
                  t.split(' ')
@@ -47,7 +47,7 @@ impl FromStr for Signal {
                  .map(|s| HashSet::from_iter(s.chars())) 
                  .into_group_map_by(|s| s.len());
         
-        Ok(Signal {digits: digits_map, outputs: vs[1].clone()})
+        Ok(Signal {digits: digits_map, outputs: vs.swap_remove(1)})
     }
 }
 
@@ -56,7 +56,7 @@ impl Signal {
         let mut deduced: HashMap<char, char> = HashMap::new();
 
         // Identify 1, 4, 7, 8
-        let uniques =
+        let uniques: Vec<_> =
             UNIQUES.iter()
                    .map(|l| {
                        self.digits
@@ -66,80 +66,78 @@ impl Signal {
                                if v.len() != 1 {
                                    Err("unique wrong number")
                                } else {
-                                   // NB: A bit lame, but I cannot return Ok(&v[0]) here because
-                                   // apparently rust thinks this ends up leaking a reference to
-                                   // self.digits to outside of deduce, and then you need to
-                                   // put lifetime specifiers
-                                   Ok(v[0].clone())
+                                   Ok(&v[0])
                                }
                            })
                    })
-                   .collect::<Result<Vec<_>, _>>()?;
+                   .try_collect()?;
         
+        fn helper<'a, 'b: 'a, F, I>(digits: &'b HashMap<usize, Vec<HashSet<char>>>, sc: usize, f: F)
+            -> Result<char, &'static str>
+        where
+            F: Fn(&'b HashSet<char>) -> I,
+            I: Iterator<Item = &'a char>
+        {
+            let val =
+                digits
+                    .get(&SEGCOUNTS[sc])
+                    .ok_or("could not find")?
+                    .iter()
+                    .map(|digit| f(digit).copied().collect_vec())
+                    .find(|v| v.len() == 1)
+                    .ok_or("could not deduce")?[0];
+            Ok(val)
+        }
+
         // Deduce a from 1 and 7
-        let a = *uniques[2].difference(&uniques[0]).next().unwrap();
+        let a = *uniques[2].difference(&uniques[0]).exactly_one().map_err(|_| "not unique")?;
         deduced.insert(a, 'a');
 
         // Deduce b, d from 4 and 0
-        let d_cands = uniques[1].difference(&uniques[0]).cloned().collect::<HashSet<_>>();
-        let d = 
-            self.digits
-                .get(&SEGCOUNTS[0])
-                .ok_or("could not find 0")?
-                .iter()
-                .map(|digit| d_cands.difference(digit).cloned().collect_vec())
-                .filter(|v| v.len() == 1)
-                .next()
-                .unwrap()[0];
-        let b = *d_cands.iter().filter(|&&cand| cand != d).next().unwrap();
+        let d_cands: HashSet<_> = uniques[1].difference(&uniques[0]).copied().collect();
+        let d = helper(&self.digits, 0, |digit| d_cands.difference(digit))?;
+        let b = *d_cands.iter().find(|&&cand| cand != d).ok_or("could not find")?;
         deduced.insert(b, 'b');
         deduced.insert(d, 'd');
 
         // Deduce c, f from 1 and 6
-        let c =
-            self.digits
-                .get(&SEGCOUNTS[6])
-                .ok_or("could not find 6")?
-                .iter()
-                .map(|digit| uniques[0].difference(digit).cloned().collect_vec())
-                .filter(|v| v.len() == 1)
-                .next()
-                .unwrap()[0];
-        let f = *uniques[0].iter().filter(|&&cand| cand != c).next().unwrap();
+        let c = helper(&self.digits, 6, |digit| uniques[0].difference(digit))?;
+        let f = *uniques[0].iter().find(|&&cand| cand != c).ok_or("could not find")?;
         deduced.insert(c, 'c');
         deduced.insert(f, 'f');
 
         // Deduce g from 9
-        let mut found: HashSet<char> = HashSet::from_iter(deduced.keys().cloned());
-        let g =
-            self.digits
-                .get(&SEGCOUNTS[9])
-                .ok_or("could not find 9")?
-                .iter()
-                .map(|digit| digit.difference(&found).cloned().collect_vec())
-                .filter(|v| v.len() == 1)
-                .next()
-                .unwrap()[0];
+        let mut found: HashSet<char> = HashSet::from_iter(deduced.keys().copied());
+        let g = helper(&self.digits, 9, |digit| digit.difference(&found))?;
         deduced.insert(g, 'g');
 
         // Deduce e
         found.insert(g);
-        let all: HashSet<char> = HashSet::from_iter("abcdefg".chars());
-        let e = all.difference(&found).cloned().next().unwrap();
+        let all = HashSet::from_iter("abcdefg".chars());
+        let e = *all.difference(&found).exactly_one().map_err(|_| "could not deduce e")?;
         deduced.insert(e, 'e');
 
         // Calculate output
         let output =
             self.outputs
                 .iter()
-                .map(|s| {
-                    s.chars().map(|ch| deduced.get(&ch).unwrap()).sorted().collect::<String>()
+                .map(|s| -> Result<String, _> {
+                    s.chars()
+                     .map(|ch| deduced.get(&ch).ok_or("char not deduced"))
+                     .sorted()
+                     .try_collect()
                 })
-                .map(|s| NUMMAP.iter().position(|&r| r == s).unwrap())
-                .fold(
+                .map(|rs| {
+                    rs.and_then(|s| {
+                        NUMMAP.iter()
+                              .position(|&r| r == s)
+                              .ok_or("could not find number")
+                    })
+                })
+                .fold_ok(
                     0,
                     |acc, x| acc * 10 + x
-                );
+                )?;
 
         Ok(output)
     }
@@ -155,9 +153,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let signals =
         process_results(
             BufReader::new(file).lines(),
-            |iter| {
+            |iter| -> Result<Vec<_>, _> {
                 iter.map(|s| s.parse::<Signal>())
-                    .collect::<Result<Vec<_>, _>>()
+                    .try_collect()
             }
         )??;
     
